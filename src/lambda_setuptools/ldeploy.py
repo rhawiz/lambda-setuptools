c3fdb410fc601b9cffb18ab3d4005c656881b31b
Rawand Hawiz <r.hawiz@geophy.com>
2017-07-30 16:32:47 +0200

    lambda function update bug

--- src/lambda_setuptools/ldeploy.py
+++ src/lambda_setuptools/ldeploy.py
@@ -138,7 +138,7 @@ class LDeploy(Command):
         lambda_client = boto3.client('lambda', region)
         print(dist_path)
         print(os.path.exists(dist_path))
-        zipfile = open(dist_path, 'wb')
+        zipfile = open(dist_path, 'rb')
         lambda_mapping = {}
         lambda_config = getattr(self.distribution, 'lambda_config', {})
         for endpoint in lambda_endpoints.keys():
